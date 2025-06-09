a7f0c12a7260feccbb06e5bed2f104ce7b0daeb0
Lưu Anh Đức <luuanhduc@MacBook-Air-cua-Luu.local>
2025-06-09 10:33:33 +0700

    fix api

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://do-an-full.vercel.app',
     credentials: true
 }
 app.use(cors(corsOptions))
